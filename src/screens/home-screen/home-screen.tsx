56ad42498448c7dd2ae4e5f281eb34a38eb075a5
matheusmichels <matheusmichels@outlook.com.br>
2021-11-19 13:53:17 -0300

    down reanimated version

--- src/screens/home-screen/home-screen.tsx
+++ src/screens/home-screen/home-screen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from '@react-navigation/native';
+import { MotiText } from '@motify/components';
 
 import { Container, Title } from './home-screen.styles';
 
@@ -7,6 +8,15 @@ export function HomeScreen() {
   return (
     <Container>
       <Title>Home Screen</Title>
+
+      <MotiText
+        from={{ opacity: 0, scale: 1 }}
+        animate={{ opacity: 1, scale: 1.5 }}
+        transition={{ duration: 500, loop: true, type: 'timing' }}
+        style={{ marginVertical: 10 }}
+      >
+        Welcome
+      </MotiText>
 
       <Link to={{ screen: 'Config' }}>Go to config</Link>
     </Container>
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
-const webpack = require('webpack');
 
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(
@@ -11,10 +10,6 @@ module.exports = async function (env, argv) {
     },
     argv
   );
-
-  // https://docs.swmansion.com/react-native-reanimated/docs/2.3.0-alpha.2/web-support/#webpack-support
-  config.entry.app = ['babel-polyfill', ...config.entry.app];
-  config.plugins = [...config.plugins, new webpack.DefinePlugin({ process: { env: {} } })];
 
   return config;
 };
